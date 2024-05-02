2f7f06697a54e61cde7e2311445d369c1301e549
Rajat Gupta <7rgdev@gmail.com>
2024-05-02 14:45:29 +0530

    Added mock urls for images

---
+++ src/utils/mock.ts
@@ -0,0 +1,6 @@
+export const fileUrls = [
+  "https://utfs.io/f/f3d925d2-a789-4285-883b-d6c846b5ef79-iuuvkw.png",
+  "https://utfs.io/f/fef9159e-fcc3-4522-a2a2-8b6be3cea0e3-2ngx40.jpg",
+  "https://utfs.io/f/b93f0e8d-0d95-41f4-b4ae-be5ab3c6f8dd-s7tits.jpeg",
+  "https://utfs.io/f/19383fbd-d3fc-4f5d-9e74-6cb66aafa40c-3tx5jq.png",
+];
